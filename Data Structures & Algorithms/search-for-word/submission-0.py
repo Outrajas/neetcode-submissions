0bb50aa8ad555741ac1a02e807fdb25f96a581f4
Ojas Apratim <154970756+Outrajas@users.noreply.github.com>
2026-07-17 15:02:21 +0530

    Add: search-for-word - submission-0

---
+++ Data Structures & Algorithms/search-for-word/submission-0.py
@@ -0,0 +1,40 @@
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        row = len(board)
+        col = len(board[0])
+        if len(word) == 0:
+            return True
+        
+        visited = set()   # track visited cells
+        
+        def find(temp, i, j):
+            # base cases
+            if temp == word:
+                return True
+            if (i < 0 or i >= row or j < 0 or j >= col 
+                or (i, j) in visited 
+                or len(temp) >= len(word) 
+                or board[i][j] != word[len(temp)]):
+                return False
+            
+            # current char matches, add to visited and temp
+            visited.add((i, j))
+            temp += board[i][j]
+            
+            # try all four directions
+            if (find(temp, i+1, j) or
+                find(temp, i-1, j) or
+                find(temp, i, j+1) or
+                find(temp, i, j-1)):
+                return True
+            
+            # backtrack
+            visited.remove((i, j))
+            return False
+        
+        # try starting from every cell
+        for i in range(row):
+            for j in range(col):
+                if find("", i, j):
+                    return True
+        return False
